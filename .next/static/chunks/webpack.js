faa87cfd941f975cbff03c629711a85471e58df6
aekarach-jo <Suphachai.P@WynnSoft-Solution.com>
2022-06-06 17:21:27 +0700

    bug apiUrl

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f3a8f61d5bb73c31"; }
+/******/ 		__webpack_require__.h = function() { return "49e68c6d3bbd0a82"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
